be63e524538133f494965e1ba618dc0ac772f950
Quang <quangvm5761@ut.edu.vn>
2025-08-18 19:59:16 +0700

    Initialize project structure in dev branch

---
+++ src/main/java/com/mafiaonline/client/ClientMain.java
@@ -0,0 +1,1 @@
+package com.mafiaonline.client; public class ClientMain { public static void main(String[] args){ System.out.println("Hello from Client"); }}
---
+++ src/main/java/com/mafiaonline/server/ServerMain.java
@@ -0,0 +1,1 @@
+package com.mafiaonline.server; public class ServerMain { public static void main(String[] args){ System.out.println("Hello from Server"); }}
