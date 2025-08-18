985a53f2425e6ca63280534cd396a064e9a62e47
Quang <quangvm5761@ut.edu.vn>
2025-08-18 21:58:28 +0700

    upload

--- src/main/java/com/mafiaonline/client/ClientMain.java
+++
@@ -1,1 +0,0 @@
-package com.mafiaonline.client; public class ClientMain { public static void main(String[] args){ System.out.println("Hello from Client"); }}
--- src/main/java/com/mafiaonline/server/ServerMain.java
+++
@@ -1,1 +0,0 @@
-package com.mafiaonline.server; public class ServerMain { public static void main(String[] args){ System.out.println("Hello from Server"); }}
